1425b29ddeb3588cb569e0f80af060da9ef61177
Cayuã <cayuaronald7@gmail.com>
2025-08-05 10:36:42 -0400

    refactor: create new comments for javascript codes

--- scripts/index.js
+++ scripts/index.js
@@ -41,28 +41,46 @@ function exit (word, container) {
 enter(workplace, workplaceContainer)
 exit(workplace, workplaceContainer)
 
+
+//Cria uma nodeList com todos os elementos que tiverem a classe cards
 const cards = document.querySelectorAll('.cards')
 
+//Função que verifica cada mudança dos elementos cards
+//entries representa a mudança e entry é cada mudança
 function checkVisibleCard(entries, observer) {
     entries.forEach(entry => {
 
+        //Entry target representa os próprios cards e ratio é a propriedade de visibilidade dos cards
+        //Ou seja a a opacidade dos cards é igual ao nível de visibilidade deles
         entry.target.style.opacity = entry.intersectionRatio;
 
+        //Se o elemento estiver visível na tela ele adiciona a classe grow-effect
         if(entry.isIntersecting) {
             entry.target.classList.add('grow-effect')
+        //Do contrário ele remove, ou seja só aplica se ele estiver visível mesmo que seja pouco
         } else {
             entry.target.classList.remove('grow-effect')
         }
     });
 }
 
+//Configurações do observer
 const observerOptions = {
+
+    //Root é representa o local que o observer vai observar
+    //Como ele não está definido, o valor padrão é toda a tela
     root: null, 
+
+    //Threshold representa o quanto os itens estão visíveis
+    //Ou o que o observer vai considerar como visível
+    //Aqui ele conta tudo para escalar com a opacidade
     threshold: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 }
 
+//Criação da função com o observer que chama a função checkVisibleCard quando uma mudança acontece
 const observer = new IntersectionObserver(checkVisibleCard, observerOptions)
 
+//Aqui o forEach começa a percorrer cada card e de fato observá-los
 cards.forEach(card => {
     observer.observe(card)
 });
